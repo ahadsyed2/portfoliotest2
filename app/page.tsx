1fa67eee037accaeb43f8e0afbc46f114d0a86fa
Ahad Syed <ahadsyed2@csus.edu>
2024-05-06 18:57:10 -0700

    updated info homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,13 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`test1`}
+        As a recent computer science graduate, I bring a solid foundation in coding with Java and C, alongside proficiency in web development and database languages such as MongoDB and SQL. 
+        My previous roles involved rigorous SSD and GPU testing for industry giants like Intel and Nvidia. 
+        With a keen interest in software engineering and information technology, I'm eager to leverage my skills and experience in dynamic professional environments.
       </p>
-      
+      <p className="mb-4">
+        Download my resume <a href="link-to-your-pdf" download>here</a>.
+      </p>
     </section>
   )
 }
