3cd98e5811a6bf94e98d6d4ed34182314254f81d
Ahad Syed <ahadsyed2@csus.edu>
2024-05-06 19:06:27 -0700

    updated info homepage2

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
 import { BlogPosts } from 'app/components/posts'
-import React, { useState, useEffect } from 'react';
-import './styles.css'; // Import your CSS file where you define background animation styles
 
 export default function Page() {
-  const [backgroundColor, setBackgroundColor] = useState('#ff0000'); // Initial background color
-  
-  // Function to generate a random hex color
-  const getRandomColor = () => {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-  }
-  
-  // Update background color every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const randomColor = getRandomColor();
-      setBackgroundColor(randomColor);
-    }, 5000); // Change color every 5 seconds
-  
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <section style={{ backgroundColor: backgroundColor }}>
+    <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
@@ -31,7 +12,7 @@ export default function Page() {
         With a keen interest in software engineering and information technology, I'm eager to leverage my skills and experience in dynamic professional environments.
       </p>
       <p className="mb-4">
-        Download my resume <a href="URL-of-your-pdf" download>here</a>.
+        Download my resume <a href="link-to-your-pdf" download>here</a>.
       </p>
     </section>
   )
